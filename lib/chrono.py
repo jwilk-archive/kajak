7c47dbd41421bdb081955366a8c3056169f2dbc3
Jakub Wilk <jwilk@jwilk.net>
2016-06-17 19:37:16 +0200

    chrono: sort imports.

--- lib/chrono.py
+++ lib/chrono.py
@@ -18,9 +18,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import datetime
 import re
 import subprocess as ipc
-import datetime
 
 def parse_date(s):
     if s == 'today':
